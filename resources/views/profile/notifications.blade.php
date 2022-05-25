b64e153979a6ff6f7d1b11d8aa56090cb9351b0c
Chachanidze29 <chachanidze29m@gmail.com>
2022-05-25 23:45:30 +0400

    ight fr fr finished for today on god no cap no twizzy

--- resources/views/profile/notifications.blade.php
+++ resources/views/profile/notifications.blade.php
@@ -17,21 +17,18 @@
                             @case('App\Notifications\Liked')
                             @case('App\Notifications\Tweeted')
                                 <p>
-                                    <a class="border-b-2 border-gray-400" href="{{url('/'.$notification->data['username'])}}">{{$notification->data['username']}}</a>
-                                    {{$notification->data['text']}} <a class="border-b-2 border-gray-400" href="{{$notification->data['postRoute']}}">Post</a>
+                                    {{$notification->data['username']}} {{$notification->data['text']}} Post
                                 </p>
                                 @break
                             @case('App\Notifications\Commented')
                                 <p>
-                                    <a class="border-b-2 border-gray-400" href="{{url('/'.$notification->data['username'])}}">{{$notification->data['username']}}</a>
-                                    {{$notification->data['text1']}} <a class="border-b-2 border-gray-400" href="{{$notification->data['commentRoute']}}">Comment</a> {{$notification->data['text2']}} <a class="border-b-2 border-gray-400" href="{{$notification->data['postRoute']}}">Post</a>
+                                    {{$notification->data['username']}} {{$notification->data['text1']}} Comment {{$notification->data['text2']}} Post
                                 </p>
                                 @break
                             @case('App\Notifications\Followed')
                             @case('App\Notifications\Unfollowed')
                                 <p>
-                                    <a class="border-b-2 border-gray-400" href="{{url('/'.$notification->data['username'])}}">{{$notification->data['username']}}</a>
-                                    {{$notification->data['text']}}
+                                    {{$notification->data['username']}} {{$notification->data['text']}}
                                 </p>
                                 @break
                         @endswitch
